8b2f88cb833a3051edfea26dbc5235f8de656c43
Fun_zil <854257920@qq.com>
2025-05-25 15:30:23 +0800

    commit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -10,6 +10,11 @@ import { navs } from './nav'
 import { mdEnhancePlugin } from "vuepress-plugin-md-enhance"
 import { componentsPlugin } from "vuepress-plugin-components"
 import { searchPlugin } from '@vuepress/plugin-search'
+import { markdownMathPlugin } from '@vuepress/plugin-markdown-math'
+import { markdownHintPlugin } from '@vuepress/plugin-markdown-hint'
+import { markdownExtPlugin } from '@vuepress/plugin-markdown-ext'
+import { markdownImagePlugin } from '@vuepress/plugin-markdown-image'
+
 
 export default defineUserConfig({
     bundler: viteBundler(),
@@ -28,19 +33,35 @@ export default defineUserConfig({
     ],
     plugins:[
         mdEnhancePlugin({
-            katex: true,  //数学公式
+            //katex: true,  //数学公式
             echarts: true,  //各种统计图
-            figure: true, //图片显示标题
+            //figure: true, //图片显示标题
             mermaid: true, //各种流程图
-            alert: true, //GFM提示功能
-            gfm: true, //github flavored Markdown
-            hint: true,
+            //alert: true, //GFM提示功能
+            //gfm: true, //github flavored Markdown
+            //hint: true,
         }), //markdown功能增强插件
         componentsPlugin({
             components:[
                 "BiliBili",
             ]
         }),
-        searchPlugin({}) //搜索引擎插件，会生成本地索引文件，若网站文章过多，建议切换成docSearch插件
+        searchPlugin({}), //搜索引擎插件，会生成本地索引文件，若网站文章过多，建议切换成docSearch插件
+        markdownMathPlugin({}), //数学公式
+        markdownExtPlugin({}), //脚注、GFM等组件
+        markdownHintPlugin({
+              hint: true,// 启用提示容器，默认启用
+              alert: true,// 启用 GFM 警告
+            }),
+        markdownImagePlugin({
+              // 启用 figure
+              figure: true,
+              // 启用图片懒加载
+              lazyload: true,
+              // 启用图片标记
+              mark: true,
+              // 启用图片大小
+              size: true,
+            }),
     ]
 })
