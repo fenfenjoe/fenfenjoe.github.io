3d78d6e0dfcf3d2c755c417202de3758f93579a5
dongyz8 <dongyz8@gdii-yueyun.com>
2026-06-23 14:53:23 +0800

    commit

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,14 +14,21 @@ import { markdownHintPlugin } from '@vuepress/plugin-markdown-hint'
 import { markdownExtPlugin } from '@vuepress/plugin-markdown-ext'
 import { markdownImagePlugin } from '@vuepress/plugin-markdown-image'
 import { markdownChartPlugin } from '@vuepress/plugin-markdown-chart'
+import { path } from '@vuepress/utils'
 //import { commentPlugin } from '@vuepress/plugin-comment'
 // Waline评论直接在 Layout.vue 中集成，不再使用 vuepress-plugin-comment-plus（不支持VuePress 2）
 
 export default defineUserConfig({
-    bundler: viteBundler(),
-    layouts: {
-        Layout: './layouts/Layout.vue',
-    },
+    bundler: viteBundler({
+        viteOptions: {
+            resolve: {
+                alias: {
+                    // 用自定义 Layout.vue 覆盖主题的 Layout 组件
+                    '@theme/Layout.vue': path.resolve(__dirname, './layouts/Layout.vue'),
+                }
+            }
+        }
+    }),
     theme: defaultTheme({
         lastUpdated: true, //文章添加最近更新时间
         logo: '/images/favicon.ico', //导航栏logo
